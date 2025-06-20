dbb0a741305f5709fd9c71f0b3c4c5b1c03e039d
asma-hesham <asmaahesham801@gmail.com>
2025-06-20 19:42:42 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,16 +1,16 @@
 class BankAccount:
-    def __init__(self, owner, balance=0):
-        self.owner = owner
-        self.balance = balance
+    def __init__(self, balance=0):
+        self.account_balance = balance
 
     def deposit(self, amount):
-        self.balance += amount
+        self.account_balance += amount
 
     def withdraw(self, amount):
-        if amount <= self.balance:
-            self.balance -= amount
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
         else:
-            print("Insufficient balance")
+            return False
 
     def display_balance(self):
-        print(self.balance)
+        print(f"Current Balance: ${self.account_balance}")
