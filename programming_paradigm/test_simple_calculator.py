25f7262d93f52070d5fc7057eea4c07548787bff
asma-hesham <asmaahesham801@gmail.com>
2025-06-20 21:08:26 +0300

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,16 +4,14 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
-        """تشغيل قبل كل test"""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
-        self.assertEqual(self.calc.add(-5, -5), -10)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 3), -3)
         self.assertEqual(self.calc.subtract(-5, -5), 0)
@@ -27,7 +25,4 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(9, 3), 3)
         self.assertEqual(self.calc.divide(-6, 2), -3)
-        self.assertIsNone(self.calc.divide(5, 0))  # القسمة على صفر ترجع None
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertIsNone(self.calc.divide(5, 0))  # division by zero
