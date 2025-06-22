7a446bbb1c6b6ac2565d9edb2c7febdcf8b05d60
Asmaa <asmaahesham801@gmail.com>
2025-06-23 00:00:32 +0300

    oop file

---
+++ OOP/main.py
@@ -0,0 +1,12 @@
+from book_class import Book
+
+def main():
+    my_book = Book("1984", "George Orwell", 1949)
+
+    print(my_book)         # __str__
+    print(repr(my_book))   # __repr__
+
+    del my_book            # __del__ gets triggered
+
+if __name__ == "__main__":
+    main()
