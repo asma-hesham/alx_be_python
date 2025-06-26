761da09c19d4a920dac11cfd07f8fcab81b9ea8a
asma-hesham <asmaahesham801@gmail.com>
2025-06-26 22:37:48 +0300

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,86 +1,34 @@
-#base class
 class Book:
-    def __init_(self, title:str , author:str ):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
 
-
-    def get_details(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
-
-#derived class
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def get_details(self):
-        return f"Book: {self.title} by {self.author} file size {self.file_size}"   
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
-
-#derived class 
-class printBook(Book):
+class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_details(self):
-        return f"Book: {self.title} by {self.author} and page count {self.page_count} "
-    
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
         self.books = []
 
     def add_book(self, book):
-     self.books.append(book)
+        self.books.append(book)
 
-
-     def List_books(self):
-         for book in self.books:
-             print(self.get_details()) 
-            
-=======
-#base class
-class Book:
-    def __init_(self, title:str, author:str ):
-        self.title = title
-        self.author = author
-
-
-    def get_details(self):
-        return f"Book: {self.title} by {self.author}"
-
-
-#derived class
-class EBook(Book):
-    def __init__(self, title, author, file_size):
-        super().__init__(title, author)
-        self.file_size = file_size
-
-    def get_details(self):
-        return f"Book: {self.title} by {self.author} file size: {self.file_size}"   
-
-
-#derived class 
-class printBook(Book):
-    def __init__(self, title, author, page_count):
-        super().__init__(title, author)
-        self.page_count = page_count
-
-    def get_details(self):
-        return f"Book: {self.title} by {self.author} and page count {self.page_count} "
-    
-
-class Library:
-    def __init__(self):
-        self.books = []
-
-    def add_book(self, book):
-     self.books.append(book)
-
-
-     def List_books(self):
-         for book in self.books:
-             print(self.get_details()) 
+    def list_books(self):
+        for book in self.books:
+            print(book)
